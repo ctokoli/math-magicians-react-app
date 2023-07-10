b656ab79b5821667eda240939daf2280e36f1aaa
ctokoli <matumasie@gamil.com>
2023-07-10 15:51:11 +0000

    fix linters error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 
+          Edit
           <code>src/App.js</code>
           and save to reload.
         </p>
