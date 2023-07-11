7c50560487c2c5f25d679803e5c07afa2581ea95
ctokoli <matumasie@gamil.com>
2023-07-11 16:38:02 +0000

    create calculator components

---
+++ src/components/Calculator.js
@@ -0,0 +1,28 @@
+function Calculator() {
+  return (
+    <div className="calculator">
+      <p className="result">0</p>
+      <button type="button" className="AC">AC</button>
+      <button type="button" className="plusnsub">+/-</button>
+      <button type="button" className="percentage">%</button>
+      <button type="button" className="division">÷</button>
+      <button type="button" className="seven">7</button>
+      <button type="button" className="eight">8</button>
+      <button type="button" className="nine">9</button>
+      <button type="button" className="x">x</button>
+      <button type="button" className="four">4</button>
+      <button type="button" className="five">5</button>
+      <button type="button" className="six">6</button>
+      <button type="button" className="substract">-</button>
+      <button type="button" className="one">1</button>
+      <button type="button" className="ywo">2</button>
+      <button type="button" className="three">3</button>
+      <button type="button" className="plus">+</button>
+      <button type="button" className="zero">0</button>
+      <button type="button" className="dot">.</button>
+      <button type="button" className="equal">=</button>
+    </div>
+  );
+}
+
+export default Calculator;
