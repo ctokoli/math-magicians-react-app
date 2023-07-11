c16e9d8e70d02076e0c3ed2039727c4a9809fdfd
ctokoli <matumasie@gamil.com>
2023-07-11 17:22:28 +0000

    styled calculator ui

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,22 +5,22 @@ function Calculator() {
       <button type="button" className="AC">AC</button>
       <button type="button" className="plusnsub">+/-</button>
       <button type="button" className="percentage">%</button>
-      <button type="button" className="division">÷</button>
+      <button type="button" className="division orange">÷</button>
       <button type="button" className="seven">7</button>
       <button type="button" className="eight">8</button>
       <button type="button" className="nine">9</button>
-      <button type="button" className="x">x</button>
+      <button type="button" className="multi orange">x</button>
       <button type="button" className="four">4</button>
       <button type="button" className="five">5</button>
       <button type="button" className="six">6</button>
-      <button type="button" className="substract">-</button>
+      <button type="button" className="substract orange">-</button>
       <button type="button" className="one">1</button>
-      <button type="button" className="ywo">2</button>
+      <button type="button" className="two">2</button>
       <button type="button" className="three">3</button>
-      <button type="button" className="plus">+</button>
-      <button type="button" className="zero">0</button>
-      <button type="button" className="dot">.</button>
-      <button type="button" className="equal">=</button>
+      <button type="button" className="plus orange">+</button>
+      <button type="button" className="zeroo">0</button>
+      <button type="button" className="dott">.</button>
+      <button type="button" className="equal orange">=</button>
     </div>
   );
 }
