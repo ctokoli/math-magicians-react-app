18ed80746f21a5a6be227bffe143d6df38680c80
ctokoli <matumasie@gamil.com>
2023-07-13 11:34:22 +0000

    Update Quotes.js

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,17 @@
-function Quotes() {
+import React, { useState, useEffect } from 'react';
+
+function DisplayQuotes() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  useEffect(() => {
+    const fetchQuotes = () => {
+        
+    } 
+  });
   return (
     <div>eee</div>
   );
 }
 
-export default Quotes;
+export default DisplayQuotes;
