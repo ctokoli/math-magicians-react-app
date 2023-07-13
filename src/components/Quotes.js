f3626efd4b6bf7f19a6b43dc6a1852a563a5164c
ctokoli <matumasie@gamil.com>
2023-07-13 12:25:54 +0000

    Update Quotes.js

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,14 +1,29 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
+import QUOTES_API from '../util/fetchAPI';
 
 function DisplayQuotes() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
+
+  const fetchQuotes = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(QUOTES_API);
+      const quotes = await response.json();
+      console.log(quotes);
+      setData(quotes);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+
+    setLoading(false);
+  };
   useEffect(() => {
-    const fetchQuotes = () => {
-        
-    } 
-  });
+    fetchQuotes();
+  }, []);
   return (
     <div>eee</div>
   );
