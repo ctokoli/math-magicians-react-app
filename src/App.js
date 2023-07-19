63375a38088976708d4b465414f783bcdd3334a0
ctokoli <matumasie@gamil.com>
2023-07-19 22:23:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import LayoutPage from './components/Layout';
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <LayoutPage />
       <Calculator />
       <Quotes />
     </>
