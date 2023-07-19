d3cb21a5789e2ae2824f8c9685849e07c8bfe51e
ctokoli <matumasie@gamil.com>
2023-07-19 23:36:55 +0000

    add classNames to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
-    <div>
-      <h2>This is the home page</h2>
+    <div className="home">
+      <h2 className="home-head">This is the home page</h2>
       <p>
         Step into a world of comfort,
         warmth, and hospitality. We are thrilled to extend a heartfelt welcome to
