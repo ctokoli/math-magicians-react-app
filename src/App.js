1ee41d62cddf93515f7f212ec3cb8bb0b0b19828
ctokoli <matumasie@gmail.com>
2023-07-21 17:06:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Calculator from './components/Calculator';
+import Calculator from './components/CalculatorRender';
 import Quotes from './components/Quotes';
 import Layout from './components/Layout';
 import HomePage from './components/Home';
