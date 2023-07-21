065dcee4060f7e271d11091b9a46c9bc6ec1679c
ctokoli <matumasie@gmail.com>
2023-07-21 11:35:06 +0000

    Delete App.test.js

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders This is the home page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/This is the home page/i);
-  expect(linkElement).toBeInTheDocument();
-});
